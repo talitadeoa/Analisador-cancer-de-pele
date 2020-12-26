71f4f7782f4d36df6d4de0513011b2f59d4be130
Talita Araujo <edtaraujo@gmail.com>
2020-12-26 01:54:34 -0300

    .

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ from keras.preprocessing.image import ImageDataGenerator
 from keras.callbacks import ReduceLROnPlateau
 from sklearn.model_selection import train_test_split
 
-# concatenating paths do dataset
+# Joining dataset paths
 base_skin_dir = os.path.join('..', '/content/ham10000/')
 
 imageid_path_dict = {os.path.splitext(os.path.basename(x))[0]: x
@@ -62,13 +62,13 @@ skin_data['image'] = skin_data['path'].map(imread)
 skin_data.isna().sum()
 skin_data['age'] = skin_data['age'].fillna(skin_data['age'].median())
 
-# Preprocessing data
+# Pre-processing data
 features = skin_data.drop(columns=['cell_type_idx'],axis=1)
 label= skin_data['cell_type_idx']
 
-x_train, x_test, y_train, y_test = train_test_split(features, label, test_size=0.20,random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(features, label, test_size=0.20,random_state=42)
 
-x_train = np.asarray(Xtrain['image'].tolist())
+x_train = np.asarray(X_train['image'].tolist())
 x_test = np.asarray(X_test['image'].tolist())
 
 x_train_mean = np.mean(x_train)
@@ -79,3 +79,83 @@ x_test_std = np.std(x_test)
 
 x_train = (x_train - x_train_mean)/x_train_std
 x_test = (x_test - x_test_mean)/x_test_std
+
+no_of_classes = len(np.unique(y_train))
+
+y_train = np_utils.to_categorical(y_train,no_of_classes)
+y_test = np_utils.to_categorical(y_test,no_of_classes)
+y_train[0] 
+
+x_train, x_valid, y_train, y_valid = train_test_split(x_train, y_train, test_size = 0.1, random_state = 2)
+x_train = x_train.reshape(x_train.shape[0], *(75, 100, 3))
+x_test = x_test.reshape(x_test.shape[0], *(75, 100, 3))
+x_valid = x_valid.reshape(x_valid.shape[0], *(75, 100, 3))
+
+x_train = x_train.astype('float32')/255
+x_valid = x_valid.astype('float32')/255
+x_test = x_test.astype('float32')/255
+x_train[0]
+
+#Predictive model
+model = Sequential() # = input
+model.add(Conv2D(filters = 16, kernel_size = 2,input_shape=(75,100,3),padding='same'))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=2))
+
+model.add(Conv2D(filters = 32,kernel_size = 2,activation= 'relu',padding='same'))
+model.add(MaxPooling2D(pool_size=2))
+
+model.add(Conv2D(filters = 64,kernel_size = 2,activation= 'relu',padding='same'))
+model.add(MaxPooling2D(pool_size=2))
+
+model.add(Conv2D(filters = 128,kernel_size = 2,activation= 'relu',padding='same'))
+model.add(MaxPooling2D(pool_size=2))
+
+model.add(Dropout(0.3)) 
+model.add(Flatten())
+model.add(Dense(150))
+model.add(Activation('relu'))
+model.add(Dropout(0.4)) 
+model.add(Dense(7,activation = 'softmax')) 
+model.summary()
+
+model.compile(loss='categorical_crossentropy',
+              optimizer='rmsprop',
+              metrics=['accuracy'])
+
+batch_size = 32
+
+checkpointer = ModelCheckpoint(filepath = 'model.hdf5', verbose = 1, save_best_only = True)
+
+history = model.fit(x_train,y_train,
+        batch_size = 32,
+        epochs=30,
+        validation_data=(x_valid, y_valid),
+        callbacks = [checkpointer],
+        verbose=2, shuffle=True)
+
+model.load_weights('model.hdf5')
+score = model.evaluate(x_test, y_test, verbose=0)
+print('\n', 'Acurácia no teste:', score[1])
+
+plt.figure(1)  
+   
+ # Accuracy log
+plt.subplot(211)  
+plt.plot(history.history['accuracy'])  
+plt.plot(history.history['val_accuracy'])  
+plt.title('model accuracy')  
+plt.ylabel('accuracy')  
+plt.xlabel('epoch')  
+plt.legend(['train', 'test'], loc='upper left')  
+   
+ # Loss log
+plt.subplot(212)  
+plt.plot(history.history['loss'])  
+plt.plot(history.history['val_loss'])  
+plt.title('model loss')  
+plt.ylabel('loss')  
+plt.xlabel('epoch')  
+plt.legend(['train', 'test'], loc='upper left')  
+plt.show()
+
